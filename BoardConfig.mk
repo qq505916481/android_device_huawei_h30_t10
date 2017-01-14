8b608fbc2b9c5e53d18e022f0b0259bb852d25d7
darkmoon <505916481@qq.com>
2017-01-14 11:26:13 +0800

    Try to fix storage mount & add FB2PNG zh_CN support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,6 +110,14 @@ TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_MAX_BRIGHTNESS := 255
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
+TW_INTERNAL_STORAGE_PATH := "/sdcard"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "sdcard"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_INCLUDE_FB2PNG := true
+TW_EXTRA_LANGUAGES := true
+TW_BUILD_ZH_CN_SUPPORT := true
+TW_DEFAULT_LANGUAGE := zh_CN
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
