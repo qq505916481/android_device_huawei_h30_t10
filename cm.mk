e0e5bdb65d8c658cbefa8a35a0a5ce6305991586
darkmoon <505916481@qq.com>
2017-12-07 21:03:33 +0800

    Revert "New branch for cm-14.1 (UNTESTED)"
    This reverts commit 2891569453d80768ee79c4004ec8b864c8d3c694.
    
    aha

--- cm.mk
+++ cm.mk
@@ -3,7 +3,7 @@
 # Release name
 PRODUCT_RELEASE_NAME := h30_t10
 
-# Inherit some common Lineage stuff.
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit from hardware-specific part of the product configuration
@@ -12,7 +12,7 @@ $(call inherit-product-if-exists, vendor/Huawei/h30_t10/h30_t10-vendor.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := h30_t10
-PRODUCT_NAME := lineage_h30_t10
+PRODUCT_NAME := cm_h30_t10
 PRODUCT_BRAND := Huawei
 PRODUCT_MODEL := MT6582
 PRODUCT_MANUFACTURER := Huawei
